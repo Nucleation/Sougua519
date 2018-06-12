48bf1147eb43f417c6d3523f100f6d1ad89e3c93
zhishen－mac <zhishen-mac@zhishenmac.local>
2018-06-12 08:38:18 +0800

    612

---
+++ sg_5/Tools/Xcache.swift
@@ -0,0 +1,13 @@
+//
+//  Xcache.swift
+//  sg_5
+//
+//  Created by zhishen－mac on 2018/6/11.
+//  Copyright © 2018年 zhishen－mac. All rights reserved.
+//
+
+import UIKit
+
+class Xcache: NSObject {
+
+}
